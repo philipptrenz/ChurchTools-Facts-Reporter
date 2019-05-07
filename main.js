d40804ac06885e04f0b1c806cd00a1b282e7772e
philipptrenz <mail@philipptrenz.de>
2019-05-07 10:06:47 +0200

    Quit app at window-all-closed event also on darwin

--- main.js
+++ main.js
@@ -39,7 +39,8 @@ app.on('ready', createWindow)
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+  //if (process.platform !== 'darwin') app.quit()
+  app.quit()
 })
 
 app.on('activate', function () {
